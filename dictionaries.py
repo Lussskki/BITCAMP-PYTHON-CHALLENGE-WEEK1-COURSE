d12d927d12f16c6904570317e83115428ff00274
Luka Guledani <monster.striker21@gmail.com>
2023-03-16 23:49:02 +0400

    week1-dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,22 @@
+# from random import choice
+# from random import shuffle
+# from random import randint
+
+
+
+
+
+
+
+# number = randint(1,50)
+# print(number)
+
+# cards = ["Jack","Ace","King"]
+# shuffle(cards)
+# for card in cards:
+#     print(card)
+
+
+# coin = choice(["Luka","Magda","Anna","Mariami"])
+
+# print(coin)
